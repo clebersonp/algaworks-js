f1c2331e6ec1010ab264cee0bd280023d36e8c42
CLEBERSON PEDREIRA PAULUCI <cleberson.pauluci@viavarejo.com.br>
2018-09-10 10:14:02 -0300

    Tipos de dados em js

---
+++ tipos_dados.js
@@ -0,0 +1,30 @@
+// números
+10;
+10.5;
+
+// strings
+"Olá";
+'Olá';
+
+//booleanos
+true;
+false;
+
+// arrays
+[10, "oi", false]; // aceita varios tipos de dados no mesmo container
+
+// objetos
+{
+	nome: "João",
+	sobrenome: " das Couves"
+}
+
+// declaração variaveis. São tipo dinamicos
+var y = 'Fulano';
+y = 21;
+
+// Outros valores
+
+var z; // valor default é undefined
+var pessoa = null; // null para objetos
+
