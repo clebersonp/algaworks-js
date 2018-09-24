7139c6b0b2424c9c5642e329b2d0e419b50937f6
Cleberson Pauluci <pauluci.cleberson@gmail.com>
2018-09-24 15:07:01 -0300

    Declaracao e hoisting de funcoes

---
+++ declaracao_funcoes.js
@@ -0,0 +1,34 @@
+// Declaração Tradicional de função - Função eh como se fosse metodo no java
+function dizerOi(nome) {
+	console.log("Oi " + nome);
+};
+
+// chamar a funcao declarada
+dizerOi("Cleberson");
+
+// Expressão de função - Cria uma função anonima e atribui a uma variavel
+var dizerOla = function (nome) {
+	console.log("Olá " + nome);
+};
+
+// chamar a função
+dizerOla("Bruna");
+
+// declarando uma funcao pelo construtor da funcao
+// o construtor recebe n parametros, o primeiro é o argumento,
+// o segundo é a instrução que sera executada dentro da função
+// maneira pouco utilizado e eh inseguro, mas se precisar executar
+// um script passado como string no construtor de função tbm funciona
+var dizerTchau = new Function("nome", "console.log('Tchau ' + nome);");
+
+dizerTchau("John");
+
+// Hoisting de declarações de funções
+
+// função tradicional sera levada para o topo e conseguimos
+// executa-la mesmo antes de declara-la
+
+// função anonima e a a função pelo construtor de função somete a variavel
+// que foi utilizada para a declaração da função será levada para o topo do arquivo.
+// se tentarmos executar uma desses funções antes de sua declaração, receberemos um erro, pois
+// as variáveis estarão como undefined ainda, igual ao hoisting de declaração de variaveis.
