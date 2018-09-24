d9d7f4cfbfede82a359cde2ce53a7e8dae94e6ed
Cleberson Pauluci <pauluci.cleberson@gmail.com>
2018-09-24 18:58:40 -0300

    datas em js

---
+++ dates.js
@@ -0,0 +1,10 @@
+// datas em js
+var hoje = new Date();
+
+console.log(hoje);
+
+var ontem = new Date("09/23/2018");
+console.log(ontem);
+
+var outroDia = new Date("01/11/2016 05:30:00");
+console.log(outroDia);
